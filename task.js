2d3e2a07a087cf46bfb15e786bdae2657d261e77
Timur <tima97855@gmail.com>
2023-10-30 20:40:04 +0600

    Second task

--- task.js
+++ task.js
@@ -2,9 +2,21 @@
 // 1) Дан массив с элементами [1, 2, 3, 4, 5]. С помощью цикла for найдите сумму элементов этого массива. 
 // Запишите ее в переменную result.
 
-let a = [1, 2, 3, 4, 5];
-let result = 0;
-for (let i = 0; i < a.length; i++) {
-  result += a[i];
+// let a = [1, 2, 3, 4, 5];
+// let result = 0;
+// for (let i = 0; i < a.length; i++) {
+//   result += a[i];
+// }
+// console.log(result);
+
+// 2) Дан массив с числами. Числа могут быть положительными и отрицательными. 
+// Найдите сумму положительных элементов массива.
+
+let arr = [2, 5, 9, 15, 0, 4, -9];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0){
+        sum += arr[i];
+    }
 }
-console.log(result);
+console.log(sum);
